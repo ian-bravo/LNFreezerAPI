2ec7aee9865c12817569307511cd7e97c4b23f3a
ian-bravo <bravo.ian@gmail.com>
2023-12-09 23:17:26 -0800

    add Models

---
+++ LNFreezerAPI/Models/Freezer.cs
@@ -0,0 +1,8 @@
+namespace LNFreezerApi.Models
+{
+  public class Freezer
+  {
+    public int FreezerId { get; set; }
+    public int FreezerNum { get; set; }
+  }
+}
---
+++ LNFreezerAPI/Models/Rack.cs
@@ -0,0 +1,9 @@
+namespace LNFreezerApi.Models
+{
+  public class Rack
+  {
+    public int RackId { get; set; }
+    public int RackNum { get; set; }
+    public int FreezerId { get; set; }
+  }
+}
