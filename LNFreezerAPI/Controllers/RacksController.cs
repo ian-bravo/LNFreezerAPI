6be0ca567c08faad60b479d8cae786c8e6ddd7c1
ian-bravo <bravo.ian@gmail.com>
2023-12-15 23:59:00 -0800

    add GET method for RacksController

---
+++ LNFreezerAPI/Controllers/RacksController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LNFreezerApi.Models;
+
+namespace LNFreezerApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class RacksController : ControllerBase
+  {
+    private readonly LNFreezerApiContext _db;
+
+    public RacksController(LNFreezerApiContext db)
+    {
+      _db = db;
+    }
+
+    //GET: api/racks
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Rack>>> Get([FromQuery] int? rackNum, [FromQuery]  int? freezerId)
+    {
+      IQueryable<Rack> query = _db.Racks.AsQueryable();
+
+      if (rackNum.HasValue)
+      {
+        query = query.Where(entry => entry.RackNum == rackNum.Value);
+      }
+
+      if (freezerId.HasValue)
+      {
+        query = query.Where(entry => entry.FreezerId == freezerId.Value);
+      }
+
+      return await query.ToListAsync();
+    }
+
+    //GET: api/racks/{id}
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Rack>> GetRack(int id)
+    {
+      Rack rack = await _db.Racks.FindAsync(id);
+
+      if (rack == null)
+      {
+        return NotFound();
+      }
+
+      return rack;
+    }
+  }
+}
